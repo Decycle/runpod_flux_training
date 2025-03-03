6aa3004372d49b6937dfcbb1936af781cb48db15
Dennis Yang <decycle.yang@gmail.com>
2025-03-02 19:11:28 -0800

    updated env for runpod

--- scripts/utils/space.py
+++ scripts/utils/space.py
@@ -2,8 +2,8 @@ import os
 import boto3
 
 def init_space():
-    RUNPOD_IMG_READ_SECRET = os.getenv("RUNPOD_IMG_READ_SECRET")
-    RUNPOD_IMG_READ_ACCESS = os.getenv("RUNPOD_IMG_READ_ACCESS")
+    SPACE_RUNPOD_IMG_READ_SECRET = os.getenv("SPACE_RUNPOD_IMG_READ_SECRET")
+    SPACE_RUNPOD_IMG_READ_ACCESS = os.getenv("SPACE_RUNPOD_IMG_READ_ACCESS")
     SPACE_URL = "https://fluxtrain.nyc3.digitaloceanspaces.com/"
 
 
@@ -11,7 +11,7 @@ def init_space():
     client = session.client('s3',
                             endpoint_url=SPACE_URL,
                             region_name='nyc3',
-                            aws_access_key_id=RUNPOD_IMG_READ_ACCESS,
-                            aws_secret_access_key=RUNPOD_IMG_READ_SECRET
+                            aws_access_key_id=SPACE_RUNPOD_IMG_READ_ACCESS,
+                            aws_secret_access_key=SPACE_RUNPOD_IMG_READ_SECRET
                             )
     return client
